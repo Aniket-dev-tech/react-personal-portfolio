399605defb6eae7be52d67052b3d773b3f3998e2
Aniket Nitnaware <nitnawareaniket167@gmail.com>
2025-08-11 02:48:52 +0530

    Fix certificate images

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -3,32 +3,32 @@ const certifications = [
   {
       id: 5,
       title: "Introduction to AI",
-      image: "./src/assets/certificates/Simplilearn-AI-Certificate.png",
+      image: "./certificates/Simplilearn-AI-Certificate.png",
   },
   {
     id: 1,
     title: "TCS Data Plus Course Completetion",
-    image: "./src/assets/certificates/TCS-Data-Plus.png",
+    image: "./certificates/TCS-Data-Plus.png",
   },
   {
     id: 2,
     title: "NVDIA's Disaster Risk and Monitoring Using Sattelite Imagery",
-    image: "./src/assets/certificates/NVDIA-certificate-1.png",
+    image: "./certificates/NVDIA-certificate-1.png",
   },
   {
     id: 2,
     title: "Deloitte Cyber Job Simulation",
-    image: "./src/assets/certificates/Deloitte-Certificate-1.png",
+    image: "./certificates/Deloitte-Certificate-1.png",
   },
   {
     id: 3,
     title: "Gen Ai Certificate Prodigy",
-    image: "./src/assets/certificates/Gen-AI.png",
+    image: "./certificates/Gen-AI.png",
   },
   {
     id: 4,
     title: "LLM Google Certificate",
-    image: "./src/assets/certificates/LLM_Google.png",
+    image: "./certificates/LLM_Google.png",
   },
   
   // Add more as needed
